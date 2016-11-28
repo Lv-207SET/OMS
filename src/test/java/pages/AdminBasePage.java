77a4823e553a0c62dcfbde8524586ef74f370254
rabaroman <rabaromandm@gmail.com>
2016-11-29 00:08:58 +0200

    Adding AdminBasePage

---
+++ src/test/java/pages/AdminBasePage.java
@@ -0,0 +1,8 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+
+public abstract class AdminBasePage extends BasePage{
+    final private WebElement administration;
+    final private WebElement userInfo;
+}

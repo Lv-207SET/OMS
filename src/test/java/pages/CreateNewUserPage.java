fe21fe97f489bfb747db1e4e3c2e68d713cb35ae
oleglavr <oleglavr@ukr.net>
2016-11-29 00:23:22 +0200

    add elements ids

--- src/test/java/pages/CreateNewUserPage.java
+++ src/test/java/pages/CreateNewUserPage.java
@@ -1,4 +1,10 @@
 package pages;
 
 public class CreateNewUserPage {
+    private final   String INPUT_LOGIN="login";
+    private final   String INPUT_FIRST_NAME="firstName";
+    private final   String INPUT_LAST_NAME="lastName";
+    private final   String INPUT_PASSWORD="password";
+    private final   String INPUT_CONFIRM_PASSWORD="confirmPassword";
+    private final   String INPUT_EMAIL="email";
 }

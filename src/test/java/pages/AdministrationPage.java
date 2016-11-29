ba4a3a8627c01673d763ae8b25d5e04dcf619c8b
Voropai_Dmytro <dimon1165@gmail.com>
2016-11-29 21:51:29 +0200

    Enums/Administration page beginning

--- src/test/java/pages/AdministrationPage.java
+++ src/test/java/pages/AdministrationPage.java
@@ -1,4 +1,32 @@
 package pages;
 
-public class AdministrationPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+/**
+ * Class represents "Administration" page
+ */
+public class AdministrationPage extends BasePage {
+
+    public static final String GO_TO_CREATE_NEW_USER_PAGE = "//div[@id='list']/a[1]";
+
+    public AdministrationPage(final WebDriver driver) {
+        super(driver);
+    }
+
+    //  User as role an "Administrator" goes from tab "Administration" to
+    //  "Create New User" page by using this method
+    public CreateNewUserPage gToCreateNewUserPage(){
+        driver.findElement(By.xpath(GO_TO_CREATE_NEW_USER_PAGE)).click();
+        return new CreateNewUserPage(this.driver);
+    }
+
+    public AdministrationPage selectFieldFilterDropdownList(){
+        WebElement selectDropdownListElement = driver.findElement(By.xpath("//select[@id='field']"));
+        final Select selectDropdownList = new Select(selectDropdownListElement);
+        selectDropdownList.selectByVisibleText("");
+        return this;
+    }
 }
--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebElement;
 
 public abstract class BasePage {
 
-    private WebDriver driver;
+    protected WebDriver driver;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;

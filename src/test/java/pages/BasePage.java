f2478b80487ce3c313de9095ddff70bf139480b4
rabaroman <rabaromandm@gmail.com>
2016-11-29 02:23:19 +0200

    Adding locators to BasePage and AdminBasePage

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,11 +1,37 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class BasePage {
-    final private WebElement logOutButton;
-    final private WebElement omsLabel;
-    final private WebElement simpleSlimGeniusLabel;
-    final private WebElement inspiredByGoogleLink;
 
+public abstract class BasePage {
+
+    private WebDriver driver;
+
+    final private String LOGOUT_BUTTON_XPATH = ".//*[@id='logout']/img";
+    final private String OMS_LABEL_XPATH= ".//*[@id='logo']/h1";
+    final private String SIMPLE_SLIM_GENIUS_LABEL_XPATH= ".//*[@id='logo']/h2";
+    final private String INSPIRED_BY_GOOGLE_LINK_XPATH= ".//*[@id='footer']/a";
+    final private String LAST_UPDATE_LABEL_XPATH= ".//*[@id='footer']/p";
+
+    public WebElement getLogoutButton(){
+        return driver.findElement(By.xpath(LOGOUT_BUTTON_XPATH));
+    }
+
+    public WebElement getOmsLabel(){
+        return driver.findElement(By.xpath(OMS_LABEL_XPATH));
+    }
+
+    public WebElement getSimpleSlimGeniusLabel(){
+        return driver.findElement(By.xpath(SIMPLE_SLIM_GENIUS_LABEL_XPATH));
+    }
+
+    public WebElement getInspiredByGoogleLink(){
+        return driver.findElement(By.xpath(INSPIRED_BY_GOOGLE_LINK_XPATH));
+    }
+
+    public WebElement getLastUpdateLabel(){
+        return driver.findElement(By.xpath(LAST_UPDATE_LABEL_XPATH));
+    }
 }

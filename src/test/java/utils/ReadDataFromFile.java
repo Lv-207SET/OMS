250e10583f79bb6d11fcb70dde593748a013b807
harasym <harasym@mail.ua>
2016-12-02 16:42:20 +0200

    add xls Data file

--- src/test/java/utils/ReadDataFromFile.java
+++ src/test/java/utils/ReadDataFromFile.java
@@ -1,6 +1,5 @@
 package utils;
 
-
 import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -16,6 +15,8 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.testng.annotations.DataProvider;
 
+import com.sun.jna.platform.win32.Netapi32Util.User;
+
 import database.ProductEntity;
 import database.UserEntity;
 import enums.Region;
@@ -28,7 +29,7 @@ public class ReadDataFromFile {
     private static HSSFSheet tableSheet;
     private static HSSFWorkbook tableBook;
     private static HSSFCell sheetCell;
-//    private static XSSFRow SheetRow;
+    // private static XSSFRow SheetRow;
 
     private static String dataFileName = "TestData.xls";
     private static String dataFilePath;
@@ -42,7 +43,9 @@ public class ReadDataFromFile {
 
     // ---read the test data from the cell--\\
 
-    public static String getSpecifficCellData(int Row, int Column) throws Exception {// todo catch exetionss
+    public static String getSpecifficCellData(int Row, int Column) throws Exception {// todo
+                                                                                     // catch
+                                                                                     // exetionss
         sheetCell = tableSheet.getRow(Row).getCell(Column);
         String CellData = sheetCell.getStringCellValue();
         return CellData;
@@ -53,7 +56,7 @@ public class ReadDataFromFile {
         return dataFilePath;
     }
 
-    private static List<Object[]> readAllTestDataFromSheet (String sheetName){
+    private static List<Object[]> readAllTestDataFromSheet(String sheetName) {
         Cell cell;
         List<Object[]> testDataList = new ArrayList<Object[]>();
 
@@ -61,20 +64,15 @@ public class ReadDataFromFile {
             openDataFile(sheetName);
             Iterator<Row> rowIterator = tableSheet.iterator();
             List<String> cellsInRowData;
-
-            while (rowIterator.hasNext())
-            {
+            while (rowIterator.hasNext()) {
                 Row row = rowIterator.next();
-                //For each row, iterate through all the columns
+                // get row
                 Iterator<Cell> cellIterator = row.cellIterator();
                 cellsInRowData = new ArrayList<>();
-
-                while (cellIterator.hasNext())
-                {
+                while (cellIterator.hasNext()) {
                     cell = cellIterator.next();
-                    //Check the cell type and format accordingly
-                    switch (cell.getCellType())
-                    {
+                    // get cell format
+                    switch (cell.getCellType()) {
                         case Cell.CELL_TYPE_NUMERIC:
                             cellsInRowData.add(String.valueOf(cell.getNumericCellValue()));
                             break;
@@ -85,63 +83,67 @@ public class ReadDataFromFile {
                 }
                 testDataList.add(cellsInRowData.toArray());
             }
-        }
-        catch (Exception e)
-        {e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return testDataList;
     }
-    
 
     private static List<UserEntity> readUesrsData(String sheetName) {
         List<Object[]> users = readAllTestDataFromSheet(sheetName);
         List<UserEntity> listOfUsers = new ArrayList<UserEntity>();
-       // TODO DELITE COMMENTS
-        // user has next fields: firstName, lastName, password, login, email, region, role
-       /* for (int field = 0; field <= 7; field ++){
-        }*/
-        
+        // TODO DELITE COMMENTS
+        // user has next fields: firstName, lastName, password, login, email,
+        // region, role
+        /*
+         * for (int field = 0; field <= 7; field ++){ }
+         */
         for (Object[] userField : users) {
-            UserEntity user = new UserEntity.Builder()
-                    .setFirstName(userField[0].toString())
-                    .setLastName(userField[1].toString())
-                    .setPassword(userField[2].toString())
-                    .setLogin(userField[3].toString())
-                    .setEmail(userField[4].toString())
-                    .setRegion(Region.getRegion(userField[5].toString()))
-                    .setRole(Role.getRole(userField[6].toString()))
+            UserEntity user = new UserEntity.Builder().setFirstName(userField[0].toString())
+                    .setLastName(userField[1].toString()).setPassword(userField[2].toString())
+                    .setLogin(userField[3].toString()).setEmail(userField[4].toString())
+                    .setRegion(Region.getRegion(userField[5].toString())).setRole(Role.getRole(userField[6].toString()))
                     .build();
             listOfUsers.add(user);
         }
+        
+        
+        // bug is somewhere here
         return listOfUsers;
     }
-    
-    private static List<ProductEntity> readProductsData(String sheetName){
+
+    private static List<ProductEntity> readProductsData(String sheetName) {
         List<Object[]> products = readAllTestDataFromSheet(sheetName);
         List<ProductEntity> listOfProducts = new ArrayList<ProductEntity>();
-        for(Object[] productField : products){
-            ProductEntity product = new ProductEntity.Builder()
-                    .setName(productField[0].toString())
-                    .setDescription(productField[1].toString())
-                    .setPrice(productField[2].toString())
-                    .build();
+        for (Object[] productField : products) {
+            ProductEntity product = new ProductEntity.Builder().setName(productField[0].toString())
+                    .setDescription(productField[1].toString()).setPrice(productField[2].toString()).build();
             listOfProducts.add(product);
         }
-        
+
         return listOfProducts;
     }
-    
-    
+
     @DataProvider(name = "getUsersDataProvider")
-    public static Iterator<UserEntity> EditUsersDataProvider() {
-        return readUesrsData( "sheetWithTCNAme").iterator();
+    public static Iterator<UserEntity> editUsersDataProvider() {
+        return readUesrsData("EditUsersData").iterator();
     }
-    
+
     @DataProvider(name = "detProductsDataProvider")
     public static Iterator<ProductEntity> excelEditUsers() {
-        return readProductsData( "sheetWithTCNAme").iterator();
+        return readProductsData("sheetWithTCNAme").iterator();
     }
-  
 
+    
+    //this method was created to test current class 
+    public static void main(String[] args) {
+       Iterator<UserEntity> users = editUsersDataProvider();
+       while (users.hasNext()){
+           UserEntity user = users.next();
+           System.out.println(user.getEmail());
+           
+       }
+
+    }
 
 }

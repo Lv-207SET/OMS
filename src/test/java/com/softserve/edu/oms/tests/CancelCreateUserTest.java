56f157724b34e91a07d6e12e922759ed9bb35173
oleglavr <oleglavr@ukr.net>
2016-12-03 14:39:03 +0200

    update tests and DBUtils

--- src/test/java/com/softserve/edu/oms/tests/CancelCreateUserTest.java
+++ src/test/java/com/softserve/edu/oms/tests/CancelCreateUserTest.java
@@ -1,24 +1,28 @@
 package com.softserve.edu.oms.tests;
 
-        import com.softserve.edu.oms.pages.AdministrationPage;
-        import com.softserve.edu.oms.pages.CreateNewUserPage;
-        import com.softserve.edu.oms.pages.LoginPage;
-       // import com.softserve.edu.oms.pages.UserInfoPage;
-        import org.testng.Assert;
-        import org.testng.annotations.Test;
+import com.softserve.edu.oms.data.DBUtils;
+import com.softserve.edu.oms.pages.AdminHomePage;
+import com.softserve.edu.oms.pages.AdministrationPage;
+import com.softserve.edu.oms.pages.CreateNewUserPage;
+import com.softserve.edu.oms.pages.LoginPage;
+// import com.softserve.edu.oms.pages.UserInfoPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-public class CancelCreateUserTest extends TestRunner{
+public class CancelCreateUserTest extends TestRunner {
     @Test
-    public void  rememberMeTest(){
+    public void rememberMeTest() {
         Assert.assertEquals(logInPage.getLoginnameInputText(), "");
-        Assert.assertEquals(logInPage.getPasswordInputText(),"");
+        Assert.assertEquals(logInPage.getPasswordInputText(), "");
         logInPage.setLoginnameInput("noneiva");
         logInPage.setPasswordInput("qwerty");
         logInPage.clickSubmitButton();
-//        UserInfoPage userInfoPage=new UserInfoPage(driver);
-//        userInfoPage.clickOnAdministrationTab();
-        AdministrationPage adminPage=new AdministrationPage(driver);
-        CreateNewUserPage createPage=adminPage.goToCreateNewUserPage();
+        AdminHomePage adminHomePage = new AdminHomePage(driver);
+        adminHomePage.clickAdministrationTab();
+        AdministrationPage adminPage = new AdministrationPage(driver);
+        CreateNewUserPage createPage = adminPage.goToCreateNewUserPage();
+        DBUtils dbUtils = new DBUtils();
+        Assert.assertNull(dbUtils.getUserByLogin("hdftwqewq"));
         createPage.setLogin("hdftwqewq")
                 .setFirstName("Rob")
                 .setLastName("Roy")
@@ -26,7 +30,7 @@ public class CancelCreateUserTest extends TestRunner{
                 .setPassword("1111")
                 .setConfirmPassword("1111")
                 .cancel();
-
+        Assert.assertNull(dbUtils.getUserByLogin("hdftwqewq"));
         //Assert.assertAll();
 
     }
--- src/test/java/com/softserve/edu/oms/tests/RememberMeTest.java
+++ src/test/java/com/softserve/edu/oms/tests/RememberMeTest.java
@@ -1,5 +1,6 @@
 package com.softserve.edu.oms.tests;
 
+import com.softserve.edu.oms.pages.HomePage;
 import com.softserve.edu.oms.tests.TestRunner;
 import org.hamcrest.CoreMatchers;
 import org.testng.Assert;
@@ -7,24 +8,26 @@ import org.testng.annotations.Test;
 //import com.softserve.edu.oms.pages.UserInfoPage;
 import com.softserve.edu.oms.pages.LoginPage;
 import org.testng.asserts.SoftAssert;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.isEmptyString;
 
 public class RememberMeTest extends TestRunner {
     private SoftAssert softAssert = new SoftAssert();
+
     @Test
-    public void  rememberMeTest(){
+    public void rememberMeTest() {
         Assert.assertEquals(logInPage.getLoginnameInputText(), "");
-        Assert.assertEquals(logInPage.getPasswordInputText(),"");
+        Assert.assertEquals(logInPage.getPasswordInputText(), "");
         logInPage.setLoginnameInput("noneiva");
         logInPage.setPasswordInput("qwerty");
         logInPage.clickgetRememberMeCheckbox();
         logInPage.clickSubmitButton();
-//        UserInfoPage userInfoPage=new UserInfoPage(driver);
-//                Assert.assertEquals(userInfoPage.getFirstNameLabel(),"noneivanka");
-//        Assert.assertEquals(userInfoPage.getLastNameLabel(), "nonehoroshko");
-//        userInfoPage.clickLogoutButton();
-        logInPage=new LoginPage(driver);
+        HomePage homePage = new HomePage(driver);
+        Assert.assertEquals(homePage.getFirstnameText(), "noneivanka");
+        Assert.assertEquals(homePage.getLastnameText(), "nonehoroshko");
+        homePage.clickLogoutButton();
+        logInPage = new LoginPage(driver);
         softAssert.assertEquals(logInPage.getLoginnameInputText(), "noneiva");
         softAssert.assertEquals(logInPage.getPasswordInputText(), "qwerty");
         softAssert.assertAll();

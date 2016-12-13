b8e355ffe49f573532ffc0313ef76628f4abdc21
rabaroman <rabaromandm@gmail.com>
2016-12-13 23:58:20 +0200

    Changing DBUtils

--- src/test/java/com/softserve/edu/oms/data/DBUtils.java
+++ src/test/java/com/softserve/edu/oms/data/DBUtils.java
@@ -22,33 +22,57 @@ public class DBUtils implements IExternalData {
 	private static final String SQL_SELECT_COLUMN_LASTNAME = "SELECT LastName FROM Users WHERE IsUserActive=1 AND LastName LIKE 'none%'";
 	private static final String SQL_SELECT_COLUMN_LOGIN = "SELECT Login FROM Users WHERE IsUserActive=1 AND Login LIKE '%none%'";
 	private static final String SQL_SELECT_COLUMN_ROLE = "SELECT r.RoleName FROM Users as u JOIN Roles as r on u.RoleRef=r.ID WHERE u.IsUserActive=1 AND r.RoleName not like '%er%'";
+	
+	private static final String SQL_DELETE_USER_LOGIN = "DELETE FROM Users WHERE Login=";
+    private static final String SQL_COUNT_ALL_USERS = "SELECT count (*) FROM Users WHERE IsUserActive=1";
+
+    private static final String SQL_SELECT_COLUMN_USER_FIVE_VALUES = "select TOP 5 Users.Login, Users.FirstName, Users.LastName,Users.Password, Users.Email, Regions.RegionName, Roles.RoleName from (Users join Regions on Users.RegionRef = Regions.ID) join Roles on Users.RoleRef = Roles.ID ORDER BY RoleRef";
+    private static final String SQL_SELECT_COLUMN_NOTEXISTED_LOGIN = "SELECT * FROM Users WHERE Login=";
+
 
 	private String username = "db207";
 	private String password = "db207";
 	private String url = "jdbc:jtds:sqlserver://127.0.0.1/Lv207OMS;instance=SQLEXPRESS;";
+	
+	
+	private Connection createConnection(){
+        Connection con;
+        try {
+            DriverManager.registerDriver(new net.sourceforge.jtds.jdbc.Driver());
+            con = DriverManager.getConnection(url, username, password);
+        } catch (Exception e) {
+            throw new RuntimeException(SQL_EXCEPTION_FOUND, e);
+        }
+        return con;
+    }
 
+    
+    private void closeConnection(Connection con, Statement st) throws Exception{
+            closeConnection(con, st, null);
+    }
+    
+    
+    private void closeConnection(Connection con, Statement st, ResultSet rs) throws Exception {
+        if (rs != null) {
+            rs.close();
+        }
+        if (st != null) {
+            st.close();
+        }
+        if (con != null) {
+            con.close();
+        }
+    }
+
+    
 	public List<List<String>> getAllCells(String absoluteFilePath, String name) {
 		List<List<String>> allCells = new ArrayList<List<String>>();
 		List<String> rowCells = null;
-		Connection con = null;
 		Statement st = null;
 		ResultSet rs = null;
 		int columnCount = 0;
-		//
-		try {
-			//DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-			DriverManager.registerDriver(new net.sourceforge.jtds.jdbc.Driver());
-			con = DriverManager.getConnection(url, username, password);
-		} catch (Exception e) {
-			throw new RuntimeException(SQL_EXCEPTION_FOUND, e);
-		}
-
-		if (con != null) {
-			System.out.println("+++Connection Successful! \n");
-		} else {
-			System.out.println("+++Connection fail \n");
-			System.exit(0);
-		}
+		Connection con = createConnection();
+      
 		try {
 			st = con.createStatement();
 			rs = st.executeQuery(SQL_SELECT_USERS);
@@ -63,15 +87,7 @@ public class DBUtils implements IExternalData {
 				allCells.add(rowCells);
 				System.out.println();
 			}
-			if (rs != null) {
-				rs.close();
-			}
-			if (st != null) {
-				st.close();
-			}
-			if (con != null) {
-				con.close();
-			}
+			closeConnection(con, st, rs);
 		} catch (Exception e) {
 			throw new RuntimeException(SQL_EXCEPTION_FOUND, e);
 		}
@@ -79,56 +95,23 @@ public class DBUtils implements IExternalData {
 	}
 
 	public void deleteUsersFromDB(){
-		Connection con = null;
 		Statement st = null;
-
-		try {
-			//DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-			DriverManager.registerDriver(new net.sourceforge.jtds.jdbc.Driver());
-			con = DriverManager.getConnection(url, username, password);
-		} catch (Exception e) {
-			throw new RuntimeException(SQL_EXCEPTION_FOUND, e);
-		}
-
-		if (con != null) {
-			System.out.println("+++Connection Successful! \n");
-		} else {
-			System.out.println("+++Connection fail \n");
-			System.exit(0);
-		}
+		Connection con = createConnection();
+ 
 		try {
 			st = con.createStatement();
 			st.execute(SQL_DELETE_USERS_FIRSTNAME);
-
+		    closeConnection(con, st);
 		} catch (Exception e) {
 			throw new RuntimeException(SQL_EXCEPTION_FOUND, e);
 		}
 	}
 
 	public List<String> getOneColumn(String nameOfColumn) {
-		List<List<String>> allCells = new ArrayList<List<String>>();
-
-		List<String> listOfOneColumn = new ArrayList<>();
-		List<String> rowCells = null;
-		Connection con = null;
+ 		List<String> listOfOneColumn = new ArrayList<>();	 
 		Statement st = null;
 		ResultSet rs = null;
-		int columnCount = 0;
-		//
-		try {
-			//DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-			DriverManager.registerDriver(new net.sourceforge.jtds.jdbc.Driver());
-			con = DriverManager.getConnection(url, username, password);
-		} catch (Exception e) {
-			throw new RuntimeException(SQL_EXCEPTION_FOUND, e);
-		}
-
-		if (con != null) {
-			System.out.println("+++Connection Successful! \n");
-		} else {
-			System.out.println("+++Connection fail \n");
-			System.exit(0);
-		}
+		Connection con = createConnection();
 		try {
 			st = con.createStatement();
 			switch (nameOfColumn) {
@@ -144,15 +127,7 @@ public class DBUtils implements IExternalData {
 				listOfOneColumn.add(rs.getString(1));
 				System.out.println(rs.getString(1));
 			}
-			if (rs != null) {
-				rs.close();
-			}
-			if (st != null) {
-				st.close();
-			}
-			if (con != null) {
-				con.close();
-			}
+		  closeConnection(con, st, rs);
 		} catch (Exception e) {
 			throw new RuntimeException(SQL_EXCEPTION_FOUND, e);
 		}
@@ -161,24 +136,11 @@ public class DBUtils implements IExternalData {
 
 	public User getUserByLogin(String login) {
 		User user = null;
-		Connection con = null;
 		Statement st = null;
 		ResultSet rs = null;
 		int columnCount;
-		try {
-			//DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-			DriverManager.registerDriver(new net.sourceforge.jtds.jdbc.Driver());
-			con = DriverManager.getConnection(url, username, password);
-		} catch (Exception e) {
-			throw new RuntimeException(SQL_EXCEPTION_FOUND, e);
-		}
-
-		if (con != null) {
-			System.out.println("+++Connection Successful! \n");
-		} else {
-			System.out.println("+++Connection fail \n");
-			System.exit(0);
-		}
+		Connection con = createConnection();
+	 
 		try {
 			st = con.createStatement();
 			String query=SQL_SELECT_USER_BY_LOGIN +"\'"+ login+"\';";
@@ -213,55 +175,121 @@ public class DBUtils implements IExternalData {
 					}
 				}
 			}
-			if (rs != null) {
-				rs.close();
-			}
-			if (st != null) {
-				st.close();
-			}
-			if (con != null) {
-				con.close();
-			}
-		} catch (java.sql.SQLException e) {
+		  closeConnection(con, st, rs);
+//		} catch (java.sql.SQLException e) {
+		}  catch (Exception e) {
 			throw new RuntimeException(SQL_EXCEPTION_FOUND, e);
 		}
 		return user;
 	}
 
+	
     public List<String> getLogins(String sqlQuery){
         List<String> logins = new ArrayList<>();
-        Connection con = null;
         Statement st = null;
         ResultSet rs = null;
-
-        try {
-            DriverManager.registerDriver(new net.sourceforge.jtds.jdbc.Driver());
-            con = DriverManager.getConnection(url, username, password);
-        } catch (Exception e) {
-            throw new RuntimeException(SQL_EXCEPTION_FOUND, e);
-        }
-
-
+        Connection con = createConnection();
+        
         try {
             st = con.createStatement();
             rs = st.executeQuery(sqlQuery);
             while (rs.next()) {
                 logins.add(rs.getString("Login"));
                 //System.out.println("getting "+rs.getString("login"));
-
             }
-            if (rs != null) {
-                rs.close();
-            }
-            if (st != null) {
-                st.close();
-            }
-            if (con != null) {
-                con.close();
-            }
+          closeConnection(con, st, rs);
         } catch (Exception e) {
             throw new RuntimeException(SQL_EXCEPTION_FOUND, e);
         }
         return logins;
+    }
+    
+    public void deleteUserFromDB(String userLogin) {
+
+        Statement st;
+        Connection con = createConnection();
+
+        try {
+            st = con.createStatement();
+            st.execute(SQL_DELETE_USER_LOGIN+ "\'" + userLogin + "\'");
+            closeConnection(con, st);
+        } catch (Exception e) {
+            throw new RuntimeException(SQL_EXCEPTION_FOUND, e);
+        }
+    }
+
+    public int countAllUsers(){ 
+
+        Statement st = null;
+        ResultSet rs = null;
+        int userCount = 0;
+
+        Connection con = createConnection();
+
+        try {
+            st = con.createStatement();
+            rs = st.executeQuery(SQL_COUNT_ALL_USERS);
+            
+            rs.next();
+            userCount = rs.getInt(1);
+            closeConnection(con, st, rs);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        
+        return userCount;
+    }
+
+    public List<User> getTopFiveUsers() {
+
+        List<User> users = new ArrayList<>();
+        Statement st;
+        ResultSet rs;
+
+        Connection con = createConnection();
+
+        try {
+            st = con.createStatement();
+            rs = st.executeQuery(SQL_SELECT_COLUMN_USER_FIVE_VALUES);
+
+            while (rs.next()) {
+                User user = new User(
+                        rs.getString(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        rs.getString(5),
+                        rs.getString(6),
+                        rs.getString(7)
+                        );
+                users.add(user);
+            }
+            closeConnection(con, st,rs);
+
+        } catch (Exception e) {
+            throw new RuntimeException(SQL_EXCEPTION_FOUND, e);
+        }
+
+        return users;
+    }
+
+    public boolean verifyThatUserIsInDB(String loginOfUser){ 
+
+        Statement st;
+        ResultSet rs;
+        boolean userIsInDB = false; 
+        Connection con = createConnection();
+
+        try{
+            st = con.createStatement();
+            rs = st.executeQuery(SQL_SELECT_COLUMN_NOTEXISTED_LOGIN + "\'" + loginOfUser + "\'");
+            userIsInDB = rs.next();
+            closeConnection(con, st,rs);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return userIsInDB;
     }
 }

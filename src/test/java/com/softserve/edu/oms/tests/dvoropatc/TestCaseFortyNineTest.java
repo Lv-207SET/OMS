3163d8808067922ddfe5f353c59759ba0dc93ef6
SeverusKyo <severussvum@gmail.com>
2016-12-15 12:48:37 +0200

    added javadoc to vbybetc tests

--- src/test/java/com/softserve/edu/oms/tests/dvoropatc/TestCaseFortyNineTest.java
+++ src/test/java/com/softserve/edu/oms/tests/dvoropatc/TestCaseFortyNineTest.java
@@ -1,64 +1,64 @@
-//package com.softserve.edu.oms.tests.dvoropatc;
-//
-//import com.softserve.edu.oms.data.IUser;
-//import com.softserve.edu.oms.data.UserRepository;
-//import com.softserve.edu.oms.pages.AdminHomePage;
-//import com.softserve.edu.oms.pages.CreateNewUserPage;
-//import org.testng.Assert;
-//import org.testng.annotations.DataProvider;
-//import org.testng.annotations.Test;
-//
-//    /*
-//    * Test verifies that validation on empty mandatory fields works while
-//    * creating new user
-//    * */
-//
-//public class TestCaseFortyNineTest extends TestRunner {
-//
-//    private static final String EXPECTED_ERROR_MESSAGE_FOR_lOGIN = "Login name cannot be blank";
-//    private static final String EXPECTED_ERROR_MESSAGE_FOR_FIRST_NAME = "First name cannot be blank";
-//    private static final String EXPECTED_ERROR_MESSAGE_FOR_LAST_NAME = "Last name cannot be blank";
-//    private static final String EXPECTED_ERROR_MESSAGE_FOR_PASSWORD = "Password cannot be shorter" +
-//            " than 4 and longer than 10 characters";
-//    private static final String EXPECTED_ERROR_MESSAGE_FOR_EMAIL_ADRESS = "Incorrect format of " +
-//            "Email Address";
-//
-//    // Provides login and password of registered user
-//    // with an "Administrator" role
-//    @DataProvider
-//    public Object[][] validUserAdministrator() {
-//        return new Object[][] {
-//                {UserRepository.get().adminUser()}
-//        };
-//    }
-//
-//    @Test(dataProvider = "validUserAdministrator", alwaysRun = true)
-//    public void verifyErrorMessagesDuringUserCreation(IUser validUserAdministrator) {
-//        AdminHomePage omsAdminHomePage = logInPage.successAdminLogin(validUserAdministrator);
-//        CreateNewUserPage omsСreateNewUserPage = omsAdminHomePage
-//                .gotoAdministrationPage()
-//                .goToCreateNewUserPage()
-//                .createUser()
-//                .alertAccept();
-//
-//    //  Assert that messages appear on page for all mandatory fields
-//        Assert.assertNotNull(omsСreateNewUserPage.getErrorLogin());
-//        Assert.assertNotNull(omsСreateNewUserPage.getErrorFirstName());
-//        Assert.assertNotNull(omsСreateNewUserPage.getErrorLastName());
-//        Assert.assertNotNull(omsСreateNewUserPage.getErrorPassword());
-//        Assert.assertNotNull(omsСreateNewUserPage.getErrorEmail());
-//
-//    //  Compare existing messages with expected
-//        Assert.assertEquals(omsСreateNewUserPage.getErrorLogin(),
-//                EXPECTED_ERROR_MESSAGE_FOR_lOGIN);
-//        Assert.assertEquals(omsСreateNewUserPage.getErrorFirstName(),
-//                EXPECTED_ERROR_MESSAGE_FOR_FIRST_NAME);
-//        Assert.assertEquals(omsСreateNewUserPage.getErrorLastName(),
-//                EXPECTED_ERROR_MESSAGE_FOR_LAST_NAME);
-//        Assert.assertEquals(omsСreateNewUserPage.getErrorPassword(),
-//                EXPECTED_ERROR_MESSAGE_FOR_PASSWORD);
-//        Assert.assertEquals(omsСreateNewUserPage.getErrorEmail(),
-//                EXPECTED_ERROR_MESSAGE_FOR_EMAIL_ADRESS);
-//    }
-//
-//}
+package com.softserve.edu.oms.tests.dvoropatc;
+
+import com.softserve.edu.oms.data.IUser;
+import com.softserve.edu.oms.data.UserRepository;
+import com.softserve.edu.oms.pages.AdminHomePage;
+import com.softserve.edu.oms.pages.CreateNewUserPage;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+    /*
+    * Test verifies that validation on empty mandatory fields works while
+    * creating new user
+    * */
+
+public class TestCaseFortyNineTest extends TestRunner {
+
+    private static final String EXPECTED_ERROR_MESSAGE_FOR_lOGIN = "Login name cannot be blank";
+    private static final String EXPECTED_ERROR_MESSAGE_FOR_FIRST_NAME = "First name cannot be blank";
+    private static final String EXPECTED_ERROR_MESSAGE_FOR_LAST_NAME = "Last name cannot be blank";
+    private static final String EXPECTED_ERROR_MESSAGE_FOR_PASSWORD = "Password cannot be shorter" +
+            " than 4 and longer than 10 characters";
+    private static final String EXPECTED_ERROR_MESSAGE_FOR_EMAIL_ADRESS = "Incorrect format of " +
+            "Email Address";
+
+    // Provides login and password of registered user
+    // with an "Administrator" role
+    @DataProvider
+    public Object[][] validUserAdministrator() {
+        return new Object[][] {
+                {UserRepository.get().adminUser()}
+        };
+    }
+
+    @Test(dataProvider = "validUserAdministrator", alwaysRun = true)
+    public void verifyErrorMessagesDuringUserCreation(IUser validUserAdministrator) {
+        AdminHomePage omsAdminHomePage = logInPage.successAdminLogin(validUserAdministrator);
+        CreateNewUserPage omsСreateNewUserPage = omsAdminHomePage
+                .gotoAdministrationPage()
+                .goToCreateNewUserPage()
+                .createUser()
+                .alertAccept();
+
+    //  Assert that messages appear on page for all mandatory fields
+        Assert.assertNotNull(omsСreateNewUserPage.getErrorLogin());
+        Assert.assertNotNull(omsСreateNewUserPage.getErrorFirstName());
+        Assert.assertNotNull(omsСreateNewUserPage.getErrorLastName());
+        Assert.assertNotNull(omsСreateNewUserPage.getErrorPassword());
+        Assert.assertNotNull(omsСreateNewUserPage.getErrorEmail());
+
+    //  Compare existing messages with expected
+        Assert.assertEquals(omsСreateNewUserPage.getErrorLogin(),
+                EXPECTED_ERROR_MESSAGE_FOR_lOGIN);
+        Assert.assertEquals(omsСreateNewUserPage.getErrorFirstName(),
+                EXPECTED_ERROR_MESSAGE_FOR_FIRST_NAME);
+        Assert.assertEquals(omsСreateNewUserPage.getErrorLastName(),
+                EXPECTED_ERROR_MESSAGE_FOR_LAST_NAME);
+        Assert.assertEquals(omsСreateNewUserPage.getErrorPassword(),
+                EXPECTED_ERROR_MESSAGE_FOR_PASSWORD);
+        Assert.assertEquals(omsСreateNewUserPage.getErrorEmail(),
+                EXPECTED_ERROR_MESSAGE_FOR_EMAIL_ADRESS);
+    }
+
+}
--- src/test/java/com/softserve/edu/oms/tests/dvoropatc/TestRunner.java
+++ src/test/java/com/softserve/edu/oms/tests/dvoropatc/TestRunner.java
@@ -1,44 +1,44 @@
-//package com.softserve.edu.oms.tests.dvoropatc;
-//
-//import com.softserve.edu.oms.pages.LoginPage;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.testng.annotations.AfterClass;
-//import org.testng.annotations.BeforeClass;
-//
-//import java.util.concurrent.TimeUnit;
-//
-//public class TestRunner {
-//
-//    private static final String PATH_TO_CHROME_DRIVER = "src/test/resources/drivers/";
-//    private static final String LOGIN_PAGE_URL = "http://localhost:8080/OMS/login.htm";
-//    private static final String WEBDRIVER_CHROME_DRIVER = "webdriver.chrome.driver";
-//    protected WebDriver driver;
-//    protected LoginPage logInPage;
-//
-//    @BeforeClass
-//    public void setUpDriver() {
-//
-//        System.setProperty(WEBDRIVER_CHROME_DRIVER, PATH_TO_CHROME_DRIVER + "chromedriver.exe");
-//
-//        driver = new ChromeDriver();
-//        driver
-//                .manage()
-//                .timeouts()
-//                .implicitlyWait(30, TimeUnit.SECONDS);
-//        driver
-//                .manage()
-//                .window()
-//                .maximize();
-//
-//        driver.get(LOGIN_PAGE_URL);
-//        logInPage = new LoginPage(driver);
-//    }
-//
-//    @AfterClass
-//    public void tearDown(){
-//        driver.quit();
-//    }
-//
-//}
-//
+package com.softserve.edu.oms.tests.dvoropatc;
+
+import com.softserve.edu.oms.pages.LoginPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestRunner {
+
+    private static final String PATH_TO_CHROME_DRIVER = "src/test/resources/drivers/";
+    private static final String LOGIN_PAGE_URL = "http://localhost:8080/OMS/login.htm";
+    private static final String WEBDRIVER_CHROME_DRIVER = "webdriver.chrome.driver";
+    protected WebDriver driver;
+    protected LoginPage logInPage;
+
+    @BeforeClass
+    public void setUpDriver() {
+
+        System.setProperty(WEBDRIVER_CHROME_DRIVER, PATH_TO_CHROME_DRIVER + "chromedriver.exe");
+
+        driver = new ChromeDriver();
+        driver
+                .manage()
+                .timeouts()
+                .implicitlyWait(30, TimeUnit.SECONDS);
+        driver
+                .manage()
+                .window()
+                .maximize();
+
+        driver.get(LOGIN_PAGE_URL);
+        logInPage = new LoginPage(driver);
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
+

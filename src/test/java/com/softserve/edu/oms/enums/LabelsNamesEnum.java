e750f2e008f92941655760f768c7d7dd827a2f93
rabaroman <rabaromandm@gmail.com>
2016-12-16 01:22:33 +0200

    Add enum for constatnts in class FindingTest and DBUtils

--- src/test/java/com/softserve/edu/oms/enums/LabelsNamesEnum.java
+++ src/test/java/com/softserve/edu/oms/enums/LabelsNamesEnum.java
@@ -1,7 +1,12 @@
 package com.softserve.edu.oms.enums;
 
 public enum LabelsNamesEnum {
-     SHOW_10_ITEMS_LINK ("Show 10 items");
+    SHOW_10_ITEMS_LINK ("Show 10 items"),
+    SEARCH_TEXT_NONE ("none"),
+    SEARCH_TEXT_ER ("er"),
+    BY_LAST_NAME ("lastName"),
+    BY_LOGIN_NAME ("login"),
+    BY_ROLE ("role");
 
     public final String name;
 

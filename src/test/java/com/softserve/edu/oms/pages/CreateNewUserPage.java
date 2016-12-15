70f2bf13c56fa4479602f8f72782a73bc5c52b79
oleglavr <oleglavr@ukr.net>
2016-12-15 20:05:14 +0200

    Created AUserDataPage

--- src/test/java/com/softserve/edu/oms/pages/CreateNewUserPage.java
+++ src/test/java/com/softserve/edu/oms/pages/CreateNewUserPage.java
@@ -12,141 +12,11 @@ import java.util.List;
 
 import static com.softserve.edu.oms.locators.CreateNewUserPageLocators.*;
 
-public class CreateNewUserPage extends ABasePage {
+public class CreateNewUserPage extends AUserDataPage {
 
     public CreateNewUserPage(WebDriver driver) {
         super(driver);
     }
-
-    // get Data
-
-    public WebElement getLoginInput() {
-        return this.driver.findElement(LOGIN_INPUT_ID.by);
-    }
-
-    public WebElement getFirstNameInput() {
-        return this.driver.findElement(FIRST_NAME_INPUT_ID.by);
-    }
-
-    public WebElement getLastNameInput() {
-        return this.driver.findElement(LAST_NAME_INPUT_ID.by);
-    }
-
-    public WebElement getPasswordInput() {
-        return  this.driver.findElement(PASSWORD_INPUT_ID.by);
-    }
-
-    public WebElement getConfirmPasswordInput() {
-        return this.driver.findElement(CONFIRM_PASSWORD_INPUT_ID.by);
-    }
-
-    public WebElement getEmailInput() {
-        return  this.driver.findElement(EMAIL_INPUT_ID.by);
-    }
-
-    public WebElement getRegionDropDown() {
-        return this.driver.findElement(REGION_SELECT_ID.by);
-    }
-
-    public List<WebElement> getRoleRadioButton() {
-        return this.driver.findElements(ROLE_RADIO_BUTTON_NAME.by);
-    }
-
-    public WebElement getCreateButton() {
-        return this.driver.findElement(CREATE_BUTTON_CSS.by);
-    }
-
-    public WebElement getCancelButton() {
-        return this.driver.findElement(CANCEL_BUTTON_CSS.by);
-    }
-
-    public WebElement getLoginErrorMessage() {
-        return this.driver.findElement(ERROR_LOGIN_ID.by);
-    }
-
-    public WebElement getFirstNameErrorMessage() {
-        return this.driver.findElement(ERROR_FIRST_NAME_ID.by);
-    }
-
-    public WebElement getLastNameErrorMessage() {
-        return this.driver.findElement(ERROR_LAST_NAME_ID.by);
-    }
-
-    public WebElement getPasswordErrorMessage() {
-        return this.driver.findElement(ERROR_PASSWORD_ID.by);
-    }
-
-    public WebElement getConfirmPasswordErrorMessage() {
-        return this.driver.findElement(ERROR_CONFIRM_PASSWORD_ID.by);
-    }
-
-    public WebElement getEmailErrorMessage() {
-        return this.driver.findElement(ERROR_EMAIL_ID.by);
-    }
-
-    // functional
-
-    public String getLoginInputText() {
-        return getLoginInput().getText();
-    }
-
-    public String getFirstNameInputText() {
-        return getFirstNameInput().getText();
-    }
-
-    public String getLastNameInputText() {
-        return getLastNameInput().getText();
-    }
-
-    public String getPasswordInputText() {
-        return getPasswordInput().getText();
-    }
-
-    public String getConfirmPasswordInputText() {
-        return getConfirmPasswordInput().getText();
-    }
-
-    public String getEmailInputText() {
-        return getEmailInput().getText();
-    }
-
-    public Select getSelectRegion() {
-        return new Select(getRegionDropDown());
-    }
-
-    public String getCreateButtonText() {
-        return getCreateButton().getText().trim();
-    }
-
-    public String getCancelButtonText() {
-        return getCancelButton().getText().trim();
-    }
-
-    public String getLoginErrorMessageText() {
-        return getLoginErrorMessage().getText();
-    }
-
-    public String getFirstNameErrorMessageText() {
-        return getFirstNameErrorMessage().getText();
-    }
-
-    public String getLastNameErrorMessageText() {
-        return getLastNameErrorMessage().getText();
-    }
-
-    public String getPasswordErrorMessageText() {
-        return getPasswordErrorMessage().getText();
-    }
-
-    public String getConfirmPasswordErrorMessageText() {
-        return getConfirmPasswordErrorMessage().getText();
-    }
-
-    public String getEmailErrorMessageText() {
-        return getEmailErrorMessage().getText();
-    }
-
-    // set Data
 
     public CreateNewUserPage setLoginInput(String login) {
         getLoginInput().clear();
@@ -154,86 +24,10 @@ public class CreateNewUserPage extends ABasePage {
         return this;
     }
 
-    public CreateNewUserPage setFirstNameInput(String firstName) {
-        getFirstNameInput().clear();
-        getFirstNameInput().sendKeys(firstName);
-        return this;
-    }
-
-    public CreateNewUserPage setLastNameInput(String lastName) {
-        getLastNameInput().clear();
-        getLastNameInput().sendKeys(lastName);
-        return this;
-    }
-
-    public CreateNewUserPage setPasswordInput(String password) {
-        getPasswordInput().clear();
-        getPasswordInput().sendKeys(password);
-        return this;
-    }
-
-    public CreateNewUserPage setConfirmPasswordInput(String confirmPassword) {
-        getConfirmPasswordInput().clear();
-        getConfirmPasswordInput().sendKeys(confirmPassword);
-        return this;
-    }
-
-    public CreateNewUserPage setEmailInput(String email) {
-        getEmailInput().clear();
-        getEmailInput().sendKeys(email);
-        return this;
-    }
-
-    public CreateNewUserPage setSelectRegion(Region region) {
-        getSelectRegion().selectByVisibleText(region.getRegionType());
-        return this;
-    }
-
-    public CreateNewUserPage setSelectRole(Role roleId) {
-        driver.findElement(By.id(roleId.getRoleId())).click();
-        return this;
-    }
-
     public CreateNewUserPage clearLoginInput() {
         getLoginInput().clear();
         return this;
     }
-
-    public CreateNewUserPage clearFirstNameInput() {
-        getFirstNameInput().clear();
-        return this;
-    }
-
-    public CreateNewUserPage clearLastNameInput() {
-        getLastNameInput().clear();
-        return this;
-    }
-
-    public CreateNewUserPage clearPasswordInput() {
-        getPasswordInput().clear();
-        return this;
-    }
-
-    public CreateNewUserPage clearConfirmPasswordInput() {
-        getConfirmPasswordInput().clear();
-        return this;
-    }
-
-    public CreateNewUserPage clearEmailInput() {
-        getEmailInput().clear();
-        return this;
-    }
-
-    public CreateNewUserPage clickCreateButton() {
-        getCreateButton().click();
-        return this;
-    }
-
-    public CreateNewUserPage clickCancelButton() {
-        getCancelButton().click();
-        return this;
-    }
-
     // business logic
     public CreateNewUserPage setLoginData(IUser user) {
         setLoginInput(user.getLoginname());
@@ -270,7 +64,6 @@ public class CreateNewUserPage extends ABasePage {
         clickCancelButton();
         return new AdministrationPage(driver);
     }
-
     public CreateNewUserPage acceptAlert() {
         driver
                 .switchTo()
@@ -280,8 +73,9 @@ public class CreateNewUserPage extends ABasePage {
     }
 
     @Override
-    public CreateNewUserPage waitForLoad(){
+    public CreateNewUserPage waitForLoad() {
         super.waitForLoad();
         return this;
     }
+
 }
--- src/test/java/com/softserve/edu/oms/pages/EditUserPage.java
+++ src/test/java/com/softserve/edu/oms/pages/EditUserPage.java
@@ -1,5 +1,8 @@
 package com.softserve.edu.oms.pages;
 
+import com.softserve.edu.oms.data.IUser;
+import com.softserve.edu.oms.enums.Region;
+import com.softserve.edu.oms.enums.Role;
 import com.softserve.edu.oms.enums.SelectRegionDropdownList;
 import com.softserve.edu.oms.enums.SelectRoleDropdownList;
 import org.openqa.selenium.By;
@@ -7,218 +10,49 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
-public class EditUserPage  extends ABasePage {
-    public static final String LABEL_EDITING_CAPTION_CSS = "#edit>h3";
-    public static final String LABEL_NO_CHANGE_CSS = "#edit>span";
-    public static final String INPUT_FIRST_NAME_ID = "firstName";
-    public static final String INPUT_LAST_NAME_ID = "lastName";
-    public static final String INPUT_PASSWORD_ID = "password";
-    public static final String INPUT_CONFIRM_PASSWORD_ID = "confirmPassword";
-    public static final String INPUT_EMAIL_ID = "email";
-    public static final String SELECT_REGION_ID = "regionID";
-    public static final String SELECT_ROLE_ID = "roleID";
-    public static final String BUTTON_SAVE_CHANGES_CSS = "input[value=\"Save changes\"]";
-    public static final String BUTTON_CANCEL_CSS = "input[value=\"Cancel\"]";
-    public static final String ERROR_FIRST_NAME_ID = "firstName.errors";
-    public static final String ERROR_LAST_NAME_ID = "lastName.errors";
-    public static final String ERROR_PASSWORD_ID = "password.errors";
-    public static final String ERROR_CONFIRM_PASSWORD_ID = "confirmPassword.errors";
-    public static final String ERROR_EMAIL_ID = "emailError";
-
-    //private WebDriver driver;
-    private WebElement labelEditingCaption;
-    private WebElement labelNoChange;
-    private WebElement inputFirstName;
-    private WebElement inputLastName;
-    private WebElement inputPassword;
-    private WebElement inputConfirmPassword;
-    private WebElement inputEmail;
-    private Select selectRegion;
-    private Select selectRole;
-    private WebElement buttonSaveChanges;
-    private WebElement buttonCancel;
-
-    private WebElement errorFirstName;
-    private WebElement errorLastName;
-    private WebElement errorPassword;
-    private WebElement errorConfirmPassword;
-    private WebElement errorEmail;
-
-    public EditUserPage(WebDriver driver) {
+public class EditUserPage  extends AUserDataPage {
+    public EditUserPage (WebDriver driver) {
         super(driver);
-        this.inputFirstName = driver.findElement(By.id(INPUT_FIRST_NAME_ID));
-        this.inputLastName = driver.findElement(By.id(INPUT_LAST_NAME_ID));
-        this.inputPassword = driver.findElement(By.id(INPUT_PASSWORD_ID));
-        this.inputConfirmPassword = driver.findElement(By.id(INPUT_CONFIRM_PASSWORD_ID));
-        this.inputEmail = driver.findElement(By.id(INPUT_EMAIL_ID));
-        this.selectRegion = new Select(driver.findElement(By.id(SELECT_REGION_ID)));
-        this.selectRole = new Select(driver.findElement(By.id(SELECT_ROLE_ID)));
-        this.buttonSaveChanges = driver.findElement(By.id(BUTTON_SAVE_CHANGES_CSS));
-        this.buttonCancel = driver.findElement(By.id(BUTTON_CANCEL_CSS));
     }
 
-    public WebDriver getDriver() {
-        return driver;
-    }
-
-    public void setDriver(WebDriver driver) {
-        this.driver = driver;
-    }
-
-
-    public WebElement getInputFirstName() {
-        return inputFirstName;
-    }
-
-    public void setInputFirstName(WebElement inputFirstName) {
-        this.inputFirstName = inputFirstName;
-    }
-
-    public WebElement getInputLastName() {
-        return inputLastName;
-    }
-
-    public void setInputLastName(WebElement inputLastName) {
-        this.inputLastName = inputLastName;
-    }
-
-    public WebElement getInputPassword() {
-        return inputPassword;
-    }
-
-    public void setInputPassword(WebElement inputPassword) {
-        this.inputPassword = inputPassword;
-    }
-
-    public WebElement getInputConfirmPassword() {
-        return inputConfirmPassword;
-    }
-
-    public void setInputConfirmPassword(WebElement inputConfirmPassword) {
-        this.inputConfirmPassword = inputConfirmPassword;
-    }
-
-    public WebElement getInputEmail() {
-        return inputEmail;
-    }
-
-    public void setInputEmail(WebElement inputEmail) {
-        this.inputEmail = inputEmail;
-    }
-
-    public Select getSelectRegion() {
-        return selectRegion;
-    }
-
-    public void setSelectRegion(Select selectRegion) {
-        this.selectRegion = selectRegion;
-    }
-
-    public Select getSelectRole() {
-        return selectRole;
-    }
-
-    public void setSelectRole(Select selectRole) {
-        this.selectRole = selectRole;
-    }
-
-    public WebElement getButtonSaveChanges() {
-        return buttonSaveChanges;
-    }
-
-    public void setButtonSaveChanges(WebElement buttonSaveChanges) {
-        this.buttonSaveChanges = buttonSaveChanges;
-    }
-
-    public WebElement getButtonCancel() {
-        return buttonCancel;
-    }
-
-    public void setButtonCancel(WebElement buttonCancel) {
-        this.buttonCancel = buttonCancel;
-    }
-
-    public EditUserPage setFirstName(String firstName) {
-        getInputFirstName().clear();
-        getInputFirstName().sendKeys(firstName);
+    public EditUserPage setLoginInput(String login) {
+        getLoginInput().clear();
+        getLoginInput().sendKeys(login);
         return this;
     }
 
-    public EditUserPage setLastName(String lastName) {
-        getInputLastName().clear();
-        getInputLastName().sendKeys(lastName);
+    public EditUserPage clearLoginInput() {
+        getLoginInput().clear();
+        return this;
+    }
+    // business logic
+    public EditUserPage setLoginData(IUser user) {
+        setFirstNameInput(user.getFirstname());
+        setLastNameInput(user.getLastname());
+        setPasswordInput(user.getPassword());
+        setConfirmPasswordInput(user.getPassword());
+        setEmailInput(user.getEmail());
+        setSelectRegion(Region.getRegion(user.getRegion()));
+        setSelectRole(Role.valueOf(user.getRole().toUpperCase()));
         return this;
     }
 
-    public EditUserPage setPassword(String password) {
-        getInputPassword().clear();
-        getInputPassword().sendKeys(password);
-        return this;
-    }
-
-    public EditUserPage setConfirmPassword(String password) {
-        getInputConfirmPassword().clear();
-        getInputConfirmPassword().sendKeys(password);
-        return this;
-    }
-
-    public EditUserPage setEmail(String email) {
-        getInputEmail().clear();
-        getInputEmail().sendKeys(email);
-        return this;
-    }
-
-    public EditUserPage selectRegion(SelectRegionDropdownList region) {
-        getSelectRegion().selectByValue(region.toString());
-        return this;
-    }
-
-    public EditUserPage selectRole(SelectRoleDropdownList role) {
-        getSelectRole().selectByValue(role.toString());
-        return this;
-    }
-
-    public EditUserPage saveChangesUser() {
-        buttonSaveChanges.click();
-        return this;
-    }
-
-    public AdministrationPage cancel() {
-        buttonCancel.click();
+    public AdministrationPage successEditUser(IUser validUser){
+        setLoginData(validUser);
+        clickCreateButton();
         return new AdministrationPage(driver);
     }
 
-    public String getEditingCaption() {
-        return labelEditingCaption.getText();
+    public AdministrationPage successEditUser(){
+        clickCreateButton();
+        return new AdministrationPage(driver);
     }
 
-    public String getNoChangeMessage() {
-        return labelNoChange.getText();
+    public AdministrationPage cancelEditUser(IUser someUser) {
+        setLoginData(someUser);
+        clickCancelButton();
+        return new AdministrationPage(driver);
     }
 
-    public String getErrorFirstName() {
-        errorFirstName = driver.findElement(By.id(ERROR_FIRST_NAME_ID));
-        return errorFirstName.getText();
-    }
-
-    public String getErrorLastName() {
-        errorLastName = driver.findElement(By.id(ERROR_LAST_NAME_ID));
-        return errorLastName.getText();
-    }
-
-    public String getErrorPassword() {
-        errorPassword = driver.findElement(By.id(ERROR_PASSWORD_ID));
-        return errorPassword.getText();
-    }
-
-    public String getErrorConfirmPassword() {
-        errorConfirmPassword = driver.findElement(By.id(ERROR_CONFIRM_PASSWORD_ID));
-        return errorConfirmPassword.getText();
-    }
-
-    public String getErrorEmail() {
-        errorEmail = driver.findElement(By.id(ERROR_EMAIL_ID));
-        return errorEmail.getText();
-    }
 }
 

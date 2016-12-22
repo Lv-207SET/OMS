67ab887010de4de269d962d123970b3f04f7e7c5
nana94 <imaidanska.a.e@gmail.com>
2016-12-22 09:31:18 +0200

    Moved Tests

--- src/test/java/com/softserve/edu/oms/tests/login/ResetButtonTest.java
+++ src/test/java/com/softserve/edu/oms/tests/login/ResetButtonTest.java
@@ -3,10 +3,14 @@ package com.softserve.edu.oms.tests.login;
 import com.softserve.edu.oms.data.IUser;
 import com.softserve.edu.oms.data.UserRepository;
 import com.softserve.edu.oms.tests.TestRunner;
+import org.hamcrest.CoreMatchers;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.yandex.qatools.allure.annotations.Step;
+
+import static com.softserve.edu.oms.enums.ErrorMessagesEnum.ERROR_MESSAGE;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * This test verifies that entered user credentials values
@@ -17,10 +21,11 @@ import ru.yandex.qatools.allure.annotations.Step;
  * @author Iryna Kyselchuk
  * @since 15.12.16
  */
-public class ResetButtonTest extends TestRunner{
+public class ResetButtonTest extends TestRunner {
 
     /**
      * Provides data for user login
+     *
      * @return user from UserRepository
      */
     @DataProvider
@@ -51,3 +56,5 @@ public class ResetButtonTest extends TestRunner{
                 .isEmpty());
     }
 }
+
+

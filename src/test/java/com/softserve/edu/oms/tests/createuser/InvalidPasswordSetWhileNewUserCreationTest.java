0a38844e4ed941cff4c013a050f6dda5f38bf16a
Bohdan Harasym <Bohdan Harasym>
2016-12-24 18:06:16 +0200

    comment undone class

--- src/test/java/com/softserve/edu/oms/tests/createuser/InvalidPasswordSetWhileNewUserCreationTest.java
+++ src/test/java/com/softserve/edu/oms/tests/createuser/InvalidPasswordSetWhileNewUserCreationTest.java
@@ -1,4 +1,6 @@
-package com.softserve.edu.oms.tests.createuser;
+/*package com.softserve.edu.oms.tests.createuser;
+
+import org.testng.annotations.BeforeMethod;
 
 import com.softserve.edu.oms.data.UserRepository;
 import com.softserve.edu.oms.pages.AdministrationPage;
@@ -6,12 +8,12 @@ import com.softserve.edu.oms.pages.CreateNewUserPage;
 import com.softserve.edu.oms.tests.TestRunner;
 
 public class InvalidPasswordSetWhileNewUserCreationTest extends TestRunner{
-	
+	@BeforeMethod
 	public void loginForTest(){
 		CreateNewUserPage createNewUserPage = loginPage
 				.successAdminLogin(UserRepository
 						.get()
-						.adminUser()).;
+						.adminUser());
 		
 		
 				
@@ -19,4 +21,4 @@ public class InvalidPasswordSetWhileNewUserCreationTest extends TestRunner{
 	}
 	
 
-}
+}*/

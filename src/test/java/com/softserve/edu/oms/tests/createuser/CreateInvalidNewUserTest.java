1e94f074c6fb62ad70f6896c23f99c3c5e12128c
nana94 <maidanska.a.e@gmail.com>
2017-01-12 22:51:14 +0200

    Add steps into tests LoginWithEmptyCredentials?, CreateInvalidUser

--- src/test/java/com/softserve/edu/oms/tests/createuser/CreateInvalidNewUserTest.java
+++ src/test/java/com/softserve/edu/oms/tests/createuser/CreateInvalidNewUserTest.java
@@ -54,9 +54,10 @@ public class CreateInvalidNewUserTest extends TestRunner {
 	 * This test verifies that error messages are shown when we creating new
 	 * user with digits in last and first name
 	 */
-	@TestCaseId("LVSETOMS-")
+	@TestCaseId("LVSETOMS-54")
 	@Severity(SeverityLevel.MINOR)
-	@Description("")
+	@Description("This test verifies that error messages are shown when we creating new" +
+			" user with digits in last and first name")
 
 	@Test(dataProvider = "invalidUsers")
 	@Step
@@ -64,21 +65,24 @@ public class CreateInvalidNewUserTest extends TestRunner {
 		IUser admin = UserRepository.get().adminUser();
 
 		// Go to create new user page and set data
+		innerStep("Go to create new user page and set data");
 		CreateNewUserPage createNewUserPage = loginPage.successAdminLogin(admin).gotoAdministrationPage()
 				.gotoCreateNewUserPage().setLoginData(user);
 
 		// Verify that correct message displays near 'First Name' field
+		innerStep("Verify that correct message displays near 'First Name' field");
 		assertThat(createNewUserPage.getFirstNameErrorMessageText(),
 				CoreMatchers.equalTo(FIRST_NAME_ERROR_MESSAGE.message));
 
 		// Verify that correct message displays near 'Last Name' field
+		innerStep("Verify that correct message displays near 'Last Name' field");
 		assertThat(createNewUserPage.getLastNameErrorMessageText(),
 				CoreMatchers.equalTo(LAST_NAME_ERROR_MESSAGE.message));
 
 		DBUtils dbUtils = new DBUtils();
 
-		// Verify that user with invalid 'First Name' and 'Last Name' is not
-		// created
+		// Verify that user with invalid 'First Name' and 'Last Name' is not created
+		innerStep("Verify that user with invalid 'First Name' and 'Last Name' is not created");
 		assertThat(dbUtils.getUserByLogin(user.getLoginname()), CoreMatchers.equalTo(null));
 	}
 
